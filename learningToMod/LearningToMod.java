8f62ea4bf61e0ef159135608c74d0dc3933b6c89
Dichlorodiphen <david.young@saintandrews.net>
2013-11-09 18:10:09 -0500

    Added Skeletal Structure

--- learningToMod/LearningToMod.java
+++ learningToMod/LearningToMod.java
@@ -1,5 +1,30 @@
 package learningToMod;
 
-public class LearningToMod {
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
+@Mod( modid = "LTM", name = "Learning to Mod", version = "0.0.1" )
+
+@NetworkMod( channels = {"LTM"}, clientSideRequired = true, serverSideRequired = true )
+
+public class LearningToMod {
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
+
+	}
 }
