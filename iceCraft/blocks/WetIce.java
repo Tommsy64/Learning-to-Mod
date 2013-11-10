200076405afddc6313ccc9229d9944deeabbf5f9
Tommsy64 <thomas.pakh@gmail.com>
2013-11-10 11:11:39 -0500

    WetIce now has 105% friction!

--- iceCraft/blocks/WetIce.java
+++ iceCraft/blocks/WetIce.java
@@ -2,6 +2,7 @@ package iceCraft.blocks;
 
 import iceCraft.IceCraft;
 import net.minecraft.block.Block;
+import net.minecraft.block.StepSound;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 
@@ -13,13 +14,14 @@ public class WetIce extends Block {
 		
 		this.setHardness(0.5F);		
 		this.setLightOpacity(3);
-		this.slipperiness = 2F;
+		this.slipperiness = 1.05F;
 		
-		this.setStepSound(Block.soundGlassFootstep);
+		StepSound soundWetIceFootstep = new StepSound("stone", 1.0F, 1.0F);
 		
-		this.setCreativeTab(IceCraft.IceCraftTab);
+		this.setStepSound(Block.soundSnowFootstep);
+		
 		this.setCreativeTab(CreativeTabs.tabAllSearch);
 		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setCreativeTab(IceCraft.IceCraftTab);
 	}
-
 }
