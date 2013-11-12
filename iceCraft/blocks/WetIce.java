729f68e9e425ed65deef5823ee9f3360c389a729
Tommsy64 <thomas.pakh@gmail.com>
2013-11-12 07:40:06 -0500

    Added wetIce texture to the wetIce block

--- iceCraft/blocks/WetIce.java
+++ iceCraft/blocks/WetIce.java
@@ -1,27 +1,38 @@
 package iceCraft.blocks;
 
 import iceCraft.IceCraft;
+import iceCraft.lib.ModInfo;
+import iceCraft.lib.config.Names;
 import net.minecraft.block.Block;
 import net.minecraft.block.StepSound;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class WetIce extends Block {
 
 	public WetIce(int id) {
 		super(id, Material.ice);
-		
-		
-		this.setHardness(0.5F);		
+
+		this.setHardness(0.5F);
 		this.setLightOpacity(3);
 		this.slipperiness = 1.05F;
-		
+
 		StepSound soundWetIceFootstep = new StepSound("stone", 1.0F, 1.0F);
-		
+
 		this.setStepSound(Block.soundSnowFootstep);
-		
+
 		this.setCreativeTab(CreativeTabs.tabAllSearch);
 		this.setCreativeTab(CreativeTabs.tabBlock);
 		this.setCreativeTab(IceCraft.IceCraftTab);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister icon) {
+		blockIcon = icon.registerIcon(ModInfo.ID.toLowerCase() + ":"
+				+ Names.wetIce_unlocalizedName);
 	}
 }
