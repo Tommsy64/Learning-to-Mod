7a7c96d059620f07065ed2e9046dc8e2cc98574e
Aledde <sisos.alex@gmail.com>
2013-11-13 16:15:32 -0500

    Recipe for Ice for IceShards
    ***Temporary***

--- iceCraft/lib/Recipes.java
+++ iceCraft/lib/Recipes.java
@@ -10,16 +10,18 @@ import net.minecraft.item.crafting.CraftingManager;
 public class Recipes {
 	
 	public static void init() { 
+		
 		CraftingManager.getInstance().addRecipe(new ItemStack(Block.ice, 1),
-				"CCC",
-				"CCC",
-				"CCC",
-				Character.valueOf('C'), Block.cobblestone);
+				"SSS",
+				"SSS",
+				"SSS",
+				Character.valueOf('S'), Items.iceShard);
 		
 		CraftingManager.getInstance().addShapelessRecipe(new ItemStack(Block.ice, 1), new ItemStack(Items.iceShard, 1),
 				new ItemStack(Items.iceShard, 1), new ItemStack(Items.iceShard, 1), new ItemStack(Items.iceShard, 1));
 		
-		
 		GameRegistry.addSmelting(Block.ice.blockID, new ItemStack(Block.waterStill), 0.1F);
+		GameRegistry.addSmelting(Items.bucketWater.blockID, new ItemStack(Items.bucket), 0.1F);
+		
 	}
 }
