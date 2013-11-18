af1dbbf0b67e674bc368edd232664be711380611
Aledde <sisos.alex@gmail.com>
2013-11-18 15:59:04 -0500

    Fixed Items type where it should have been Item

--- iceCraft/lib/Recipes.java
+++ iceCraft/lib/Recipes.java
@@ -1,11 +1,11 @@
 package iceCraft.lib;
 
 import iceCraft.items.Items;
-import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Recipes {
 	
@@ -21,7 +21,7 @@ public class Recipes {
 				new ItemStack(Items.iceShard, 1), new ItemStack(Items.iceShard, 1), new ItemStack(Items.iceShard, 1));
 		
 		GameRegistry.addSmelting(Block.ice.blockID, new ItemStack(Block.waterStill), 0.1F);
-		GameRegistry.addSmelting(Items.bucketWater.blockID, new ItemStack(Items.bucket), 0.1F);
+		GameRegistry.addSmelting(Item.bucketWater.itemID, new ItemStack(Item.bucketEmpty), 0.1F);
 		
 	}
 }
