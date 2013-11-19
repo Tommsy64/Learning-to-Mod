b77dfa0547a9b7713e32b279962000b9ee9329c3
Tommsy64 <thomas.pakh@gmail.com>
2013-11-18 20:42:26 -0500

    Added a FuelHandler

---
+++ iceCraft/lib/IceCraftFuelHandler.java
@@ -0,0 +1,14 @@
+package iceCraft.lib;
+
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.IFuelHandler;
+
+public class IceCraftFuelHandler implements IFuelHandler {
+
+	@Override
+	public int getBurnTime(ItemStack fuel) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
