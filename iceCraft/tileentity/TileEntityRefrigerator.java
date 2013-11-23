1f7257923a68424d08d4b4a9718dacb63bc4cc8e
Tommsy64 <thomas.pakh@gmail.com>
2013-11-22 20:09:04 -0500

    Finished adding the tileEntity saving methods to the Refrigerator

--- iceCraft/tileentity/TileEntityRefrigerator.java
+++ iceCraft/tileentity/TileEntityRefrigerator.java
@@ -4,6 +4,8 @@ import iceCraft.lib.config.Names;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityRefrigerator extends TileEntity implements IInventory {
@@ -12,6 +14,46 @@ public class TileEntityRefrigerator extends TileEntity implements IInventory {
 
 	public TileEntityRefrigerator() {
 		inventory = new ItemStack[9];
+	}
+
+	private final String nbtName = "SlotRefrigerator";
+
+	@Override
+	public void writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+
+		NBTTagList list = new NBTTagList();
+
+		for (int i = 0; i < getSizeInventory(); i++) {
+			ItemStack itemstack = getStackInSlot(i);
+
+			if (itemstack != null) {
+				NBTTagCompound item = new NBTTagCompound();
+
+				item.setByte(nbtName, (byte) i);
+				itemstack.writeToNBT(item);
+				list.appendTag(item);
+			}
+		}
+
+		compound.setTag(nbtName, list);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+
+		NBTTagList list = compound.getTagList(nbtName);
+
+		for (int i = 0; i < list.tagCount(); i++) {
+			NBTTagCompound item = (NBTTagCompound) list.tagAt(i);
+			int slot = item.getByte(nbtName);
+
+			if (slot >= 0 && slot < getSizeInventory()) {
+				setInventorySlotContents(slot,
+						ItemStack.loadItemStackFromNBT(item));
+			}
+		}
 	}
 
 	@Override
