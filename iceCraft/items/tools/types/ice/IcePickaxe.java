e98bd45becc15335f521dff0759abb06d3a79a31
Tommsy64 <thomas.pakh@gmail.com>
2013-11-26 07:52:02 -0500

    Hitting mobs with and Ice Pickaxe now does 10 durability damage

--- iceCraft/items/tools/types/ice/IcePickaxe.java
+++ iceCraft/items/tools/types/ice/IcePickaxe.java
@@ -2,12 +2,21 @@ package iceCraft.items.tools.types.ice;
 
 import iceCraft.items.tools.lib.CustomPickaxe;
 import iceCraft.lib.config.Names;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemStack;
 
 public class IcePickaxe extends CustomPickaxe {
 
 	public IcePickaxe(int id, EnumToolMaterial toolMaterial) {
 		super(id, toolMaterial);
 		this.setUnlocalizedName(Names.icePickaxe_unlocalizedName);
+	}
+	
+	public boolean hitEntity(ItemStack stack,
+			EntityLivingBase par2EntityLivingBase,
+			EntityLivingBase par3EntityLivingBase) {
+		stack.damageItem(10, par3EntityLivingBase);
+		return true;
 	}
 }
