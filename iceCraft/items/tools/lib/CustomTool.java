1275f328940433cd76da5cf557ca427fb6e9affc
Tommsy64 <thomas.pakh@gmail.com>
2013-11-26 07:43:17 -0500

    Added Ice Hoe. Removed unnecessary setCreativeTabs in Custom item classes

--- iceCraft/items/tools/lib/CustomTool.java
+++ iceCraft/items/tools/lib/CustomTool.java
@@ -15,7 +15,6 @@ public class CustomTool extends ItemTool {
 	protected CustomTool(int id, float damageVsEntity,
 			EnumToolMaterial toolMaterial, Block[] blocksEffectiveAgainst) {
 		super(id, damageVsEntity, toolMaterial, blocksEffectiveAgainst);
-		this.setCreativeTab(CreativeTabs.tabAllSearch);
 		this.setCreativeTab(CreativeTabs.tabTools);
 		this.setCreativeTab(IceCraft.IceCraftTab);
 		this.setMaxStackSize(1);
