dc37954688eb7e3ecc35e2dab864fdb071ffdc09
Tommsy64 <thomas.pakh@gmail.com>
2013-11-25 20:46:59 -0500

    Fixed bug where IceSword extended CustomSpade instead of CustomSword

--- iceCraft/items/tools/types/ice/IceSword.java
+++ iceCraft/items/tools/types/ice/IceSword.java
@@ -1,10 +1,10 @@
 package iceCraft.items.tools.types.ice;
 
-import iceCraft.items.tools.lib.CustomSpade;
+import iceCraft.items.tools.lib.CustomSword;
 import iceCraft.lib.config.Names;
 import net.minecraft.item.EnumToolMaterial;
 
-public class IceSword extends CustomSpade {
+public class IceSword extends CustomSword {
 
 	public IceSword(int id, EnumToolMaterial toolMaterial) {
 		super(id, toolMaterial);
